4e1dfa6c0aedf16ff57a9e9c041ac444761ba787
Nógrádi Adrián <h3757046@stud.u-szeged.hu>
2025-03-22 20:34:46 +0100

    Jpg képek kicserélése Webp képkre

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 function startslider() {
-    const imgPaths = ["imgs/cae73ca4d0a568b6f5f311eda67afe55.jpg", "imgs/520015bccb572315045d1de483e381a6.jpg", "imgs/406189053ea99d037bf5b00bd489eac8.jpg", "imgs/c6bf2941acff1b5fab19ac98e60acf8f.jpg", "imgs/fdf20800875ad67c160fac8e4a0ca7a7.jpg"];
+    const imgPaths = ["imgs/cae73ca4d0a568b6f5f311eda67afe55.webp", "imgs/520015bccb572315045d1de483e381a6.webp", "imgs/406189053ea99d037bf5b00bd489eac8.webp", "imgs/c6bf2941acff1b5fab19ac98e60acf8f.webp", "imgs/fdf20800875ad67c160fac8e4a0ca7a7.webp"];
     const counter = document.getElementById("image_count");
     const slider = document.getElementById("image_slider_image");
     let currImg = 0;
